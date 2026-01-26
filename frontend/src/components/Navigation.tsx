fd8bf1f6c3e710097f602bc4b74342fc3210ca21
Kitti0403 <annus.kitti@gmail.com>
2026-01-26 11:22:47 +0100

    images into public

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -10,6 +10,7 @@ const Navigation = () => {
 
   return (
     <div className="w-full absolute top-0 left-0 bg-[#333446] z-50 shadow-md">
+      <img src="../../public/images/BookHuntNavbar.png" className="m-0 p-0 absolute " alt="BookHunt logo" />
       <nav className="flex flex-wrap justify-center gap-8 text-2xl p-4 max-w-6xl mx-auto">
         {routes.map((r) => (
           <NavLink
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,10 +5,10 @@ const Home = () => {
     <div className="min-h-screen">
       <Navigation />
       {/* Placeholder for main content of the home page */}
-      <div className="pt-20 max-w-6xl mx-auto p-4">
-        <h1 className="text-4xl font-bold mb-4">Welcome</h1>
+      <div className="pt-15 max-w-6xl mx-auto p-4">
+        <h1 className="text-4xl font-bold mb-4">Hey hey</h1>
         <p className="text-lg text-gray-700">
-          This is currently the placeholder for the main content of the home page.
+          Currently the placeholder to test routing and navigation
         </p>
     </div>
     </div>
