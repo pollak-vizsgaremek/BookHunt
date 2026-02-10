db8fe10e2012e911d31ddf975d9667818bcdc21f
DobaSandor <dobasandor83@gmail.com>
2026-02-10 13:02:41 +0100

    Fixes

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -7,17 +7,27 @@ const Navigation = () => {
     },
     {
       name: "Login",
+<<<<<<< Updated upstream
       route: "/login",
     },
     {
       name: "Register",
       route: "/register",
     },
+=======
+      route: "/Login",
+    },
+    // Add more routes here
+>>>>>>> Stashed changes
   ];
   {/* I'll merge these two later with a profile page */ }
   return (
     <div className="w-full absolute top-0 left-0 bg-[#333446] z-50 shadow-md">
-      <img src="../../public/images/BookHuntNavbar.png" className="m-0 p-0 absolute " alt="BookHunt logo" />
+      <img
+        src="../../public/images/BookHuntNavbar.png"
+        className="m-0 p-0 absolute "
+        alt="BookHunt logo"
+      />
       <nav className="flex flex-wrap justify-center gap-8 text-2xl p-4 max-w-6xl mx-auto">
         {routes.map((r) => (
           <NavLink
